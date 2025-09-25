c3607944b43f17065df46c249474cd055bf81144
emi26 <emilia.ed7@gmail.com>
2025-09-25 03:20:55 -0400

    feat: adding starter code to junit assignment repo

---
+++ Desktop/School Things/AP CSA java files/learning_unittesting/src/main/java/apcsa/githubtrack/Television.java
@@ -0,0 +1,3 @@
+package apcsa.githubtrack;
+
+// TODO: Implement the Television class here
---
+++ Desktop/School Things/AP CSA java files/learning_unittesting/src/test/java/apcsa/githubtrack/AppTest.java
@@ -0,0 +1,11 @@
+package apcsa.githubtrack;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+// TODO: Test the Television and Remote classes here
+public class AppTest {
+}
